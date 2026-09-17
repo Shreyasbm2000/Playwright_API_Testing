fd4371923cfc97f67138e8da8a9d6634056317a6
Shreyas <shreyasbm2000@gmail.com>
2026-09-18 01:42:53 +0530

    Schema Validation

--- tests/smoketest.spec.ts
+++ tests/smoketest.spec.ts
@@ -2,6 +2,8 @@ import { test } from '../utils/fixtures'
 import { expect } from '../utils/cutom-expect1'
 import { APILogger } from '../utils/logger';
 import {createToken} from '../helpers/createToken'
+import { validateSchema } from '../utils/schemaValidator';
+import articleCreatePayload from '../request-Objects/POST_article.json'
 
 
 // let authToken: string;
@@ -31,35 +33,34 @@ test("get Articles ", async ({ api }) => {
         .params({ limit: 10, offset: 0 })
         .clearAuth()
         .getRequest(200)
-    console.log(response)
+   // console.log(response)
+ await expect(response).shouldMatchSchema('articles','GET_articles',) 
     expect(response.articles.length).shouldBeLessThanOrEqual(10);
     expect(response.articlesCount).shouldEqual(10)
-
-    const response1 = await api.path("/tags/")
-        .getRequest(200);
-    expect(response1.tags[0]).shouldEqual('Test')
-    expect(response1.tags.length).shouldBeLessThanOrEqual(10)
-
 
 })
 
 test("Get Test Tags", async ({ api }) => {
     const response = await api.path("/tags/")
         .getRequest(200);
+      await expect(response).shouldMatchSchema('tags','GET_tags',true) 
+        //await validateSchema('tags','GET_tags',response)
     expect(response.tags[0]).shouldEqual('Test')
     expect(response.tags.length).shouldBeLessThanOrEqual(10)
+   // console.log(response)
 })
 
 test("Create and delete articles", async ({ api }) => {
     const createArticleResponse = await api.path('/articles/')
-        .body({ "article": { "title": "Test 1", "description": "Test Article 2", "body": "Test Body 2" } })
+        .body(articleCreatePayload)
         .postRequest(201)
-
+     await expect(createArticleResponse).shouldMatchSchema('articles','POST_article') 
     const slugID = await createArticleResponse.article.slug;
     expect(createArticleResponse.article.title).shouldEqual("Test 1");
 
     const getArticlesResponse = await api.path('/articles/')
         .getRequest(200)
+    await expect(createArticleResponse).shouldMatchSchema('articles','GET_article')     
     expect(getArticlesResponse.articles[0].title).shouldEqual('Test 1')
 
     const deleteArticelResponse = await api.path(`/articles/${slugID}`)
@@ -67,6 +68,7 @@ test("Create and delete articles", async ({ api }) => {
 
     const getArticlesResponse2 = await api.path('/articles/')
         .getRequest(200)
+    await expect(createArticleResponse).shouldMatchSchema('articles','GET_article') 
     expect(getArticlesResponse2.articles[0].title).not.shouldEqual('Test 1')
 
 })
@@ -76,19 +78,25 @@ test("Create, Update and delete articles", async ({ api }) => {
     const createArticleResponse = await api.path('/articles/')
         .body({ "article": { "title": "Test 2", "description": "Test Article 2", "body": "Test Body 2" } })
         .postRequest(201)
+     await expect(createArticleResponse).shouldMatchSchema('articles','POST_article') 
 
             const slugID = await createArticleResponse.article.slug;
     expect(createArticleResponse.article.title).shouldEqual("Test 2");
 
     const getArticlesResponse = await api.path('/articles/')
         .getRequest(200)
+    await expect(createArticleResponse).shouldMatchSchema('articles','GET_article') 
     expect(getArticlesResponse.articles[0].title).shouldEqual('Test 2')
 
     const updateArticleResponse = await api.path(`/articles/${slugID}`)
         .body({ "article": { "title": "Test 2 Re modified", "description": "Modified Test 2 Re modified", "body": "modified Body", "tagList": [], "slug": `${slugID}` } })
         .putRequest(200)
+     await expect(createArticleResponse).shouldMatchSchema('articles','PUT_article') 
+
     const getUpdatedArticlesResponse = await api.path('/articles/')
         .getRequest(200)
+    await expect(createArticleResponse).shouldMatchSchema('articles','GET_article') 
+
     expect(getUpdatedArticlesResponse.articles[0].title).shouldEqual('Test 2 Re modified')
 
         const modifiedSLugID = getUpdatedArticlesResponse.articles[0].slug;
@@ -98,6 +106,7 @@ test("Create, Update and delete articles", async ({ api }) => {
 
     const getArticlesResponse2 = await api.path('/articles/')
         .getRequest(200)
+    await expect(createArticleResponse).shouldMatchSchema('articles','GET_article') 
     expect(getArticlesResponse2.articles[0].title).not.shouldEqual('Test 2 Re modified')
 
 })
--- utils/cutom-expect1.ts
+++ utils/cutom-expect1.ts
@@ -1,5 +1,7 @@
 import { expect as baseExpect } from '@playwright/test';
 import { APILogger } from './logger';
+import { validateSchema } from './schemaValidator';
+import { promises } from 'dns';
 
 
 let apiLogger: APILogger
@@ -10,16 +12,41 @@ export const setCustomExpectLogger = (logger: APILogger) => {
 
 
 declare global {
-    namespace PlaywrightTest{
-        interface Matchers<R,T>{
-           shouldEqual(expected:T) :R ,
-           shouldBeLessThanOrEqual(expected:T):R
+    namespace PlaywrightTest {
+        interface Matchers<R, T> {
+            shouldEqual(expected: T): R,
+            shouldBeLessThanOrEqual(expected: T): R
+            shouldMatchSchema(dirName: string, fileName: string, createSchemaFlag?: boolean): Promise<R>
         }
     }
 }
 
 
 export const expect = baseExpect.extend({
+// If we want tot generate schema for all the resposnes we need not to  add true in all the expect we can just flip the createSchemaFlag
+    async shouldMatchSchema(recevied: any, dirName: string, fileName: string, createSchemaFlag: boolean = false) {
+        let pass: boolean;
+        let message: string = "";
+
+        try {
+            await validateSchema(dirName, fileName, recevied, createSchemaFlag)
+
+            pass = true;
+            message = `Schema Validation Passed`
+        } catch (e: any) {
+            pass = false;
+
+            const logs = apiLogger.getRecentLogs()
+            message = `${e.message}\n\n Recent API Activity: \n${logs}`
+        }
+
+        return {
+            message: () => message,
+            pass,
+
+        };
+    },
+
 
     shouldEqual(recevied: any, expected: any) {
         let pass: boolean;
@@ -28,8 +55,8 @@ export const expect = baseExpect.extend({
         try {
             baseExpect(recevied).toEqual(expected);
             pass = true;
-            if(this.isNot){
-                           logs = apiLogger.getRecentLogs() 
+            if (this.isNot) {
+                logs = apiLogger.getRecentLogs()
             }
         } catch (e: any) {
             pass = false;
@@ -39,13 +66,13 @@ export const expect = baseExpect.extend({
         const message = this.utils.matcherHint('shouldEqual', undefined, undefined, { isNot: this.isNot }) +
             '\n\n' +
             `Expected: ${hint} ${this.utils.printExpected(expected)}\n` +
-            `Recevied: ${this.utils.printReceived(recevied)}\n\n`+
+            `Recevied: ${this.utils.printReceived(recevied)}\n\n` +
             `Recent API Activity: \n${logs}`
 
         return {
-            message:  ()=>message,
+            message: () => message,
             pass,
-           
+
         };
     },
     shouldBeLessThanOrEqual(recevied: any, expected: any) {
@@ -55,8 +82,8 @@ export const expect = baseExpect.extend({
         try {
             baseExpect(recevied).toEqual(expected);
             pass = true;
-            if(this.isNot){
-                           logs = apiLogger.getRecentLogs() 
+            if (this.isNot) {
+                logs = apiLogger.getRecentLogs()
             }
         } catch (e: any) {
             pass = false;
@@ -66,13 +93,13 @@ export const expect = baseExpect.extend({
         const message = this.utils.matcherHint('shouldBeLessThanOrEqual', undefined, undefined, { isNot: this.isNot }) +
             '\n\n' +
             `Expected: ${hint} ${this.utils.printExpected(expected)}\n` +
-            `Recevied: ${this.utils.printReceived(recevied)}\n\n`+
+            `Recevied: ${this.utils.printReceived(recevied)}\n\n` +
             `Recent API Activity: \n${logs}`
 
         return {
-            message:  ()=>message,
+            message: () => message,
             pass,
-           
+
         };
     }
 
